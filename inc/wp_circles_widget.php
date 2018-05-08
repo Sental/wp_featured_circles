c599b78ccf52bc264294d2b227836547fcfc47b2
Sental <35261502+Sental@users.noreply.github.com>
2018-05-08 18:17:23 +0100

    Update wp_circles_widget.php

--- inc/wp_circles_widget.php
+++ inc/wp_circles_widget.php
@@ -1,142 +1,123 @@
-Skip to content
-This repository
-Search
-Pull requests
-Issues
-Marketplace
-Explore
- @Sental
-Sign out
-2
-3 0 simonrcodrington/Introduction-to-WordPress-Plugins---Location-Plugin
- Code  Issues 0  Pull requests 0  Projects 0  Wiki  Insights
-Introduction-to-WordPress-Plugins---Location-Plugin/wp_simple_location_plugin/inc/wp_location_widget.php
-3ece815  on Aug 9, 2015
- Simon Codrington Adding all files to the repo.
-     
-116 lines (98 sloc)  3.67 KB
 <?php
-/*
- * Wp location Widget
- * Defines the widget to be used to showcase single or multiple locations
- */
+defined( 'ABSPATH' ) or die( 'Nope, not accessing this' );
 //main widget used for displaying locations
-class wp_location_widget extends WP_widget{
-	
-	//initialise widget values
-	public function __construct(){
-		//set base values for the widget (override parent)
-		parent::__construct(
-			'wp_location_widget',
-			'WP Location Widget', 
-			array('description' => 'A widget that displays your locations')
-		);
-		add_action('widgets_init',array($this,'register_wp_location_widgets'));
-	}
-	
-	//handles public display of the widget
-	//$args - arguments set by the widget area, $instance - saved values
-	public function widget( $args, $instance ) {
-		
-		//get wp_simple_location class (as it builds out output)
-		global $wp_simple_locations;
-		
-		//pass any arguments if we have any from the widget
-		$arguments = array();
-		//if we specify a location
-		
-		//if we specify a single location
-		if($instance['location_id'] != 'default'){
-			$arguments['location_id'] = $instance['location_id'];
-		}
-		//if we specify a number of locations
-		if($instance['number_of_locations'] != 'default'){
-			$arguments['number_of_locations'] = $instance['number_of_locations'];
-		}
-		
-		//get the output
-		$html = '';
-		
-		$html .= $args['before_widget'];
-		$html .= $args['before_title'];
-		$html .= 'Locations';
-		$html .= $args['after_title'];
-		//uses the main output function of the location class
-		$html .= $wp_simple_locations->get_locations_output($arguments);
-		$html .= $args['after_widget'];
-		
-		echo $html;
-	}
-	
-	//handles the back-end admin of the widget
-	//$instance - saved values for the form
-	public function form($instance){
-		//collect variables 
-		$location_id = (isset($instance['location_id']) ? $instance['location_id'] : 'default');
-		$number_of_locations = (isset($instance['number_of_locations']) ? $instance['number_of_locations'] : 5);
-		
-		?>
-		<p>Select your options below</p>
-		<p>
-			<label for="<?php echo $this->get_field_name('location_id'); ?>">Location to display</label>
-			<select class="widefat" name="<?php echo $this->get_field_name('location_id'); ?>" id="<?php echo $this->get_field_id('location_id'); ?>" value="<?php echo $location_id; ?>">
-				<option value="default">All Locations</option>
-				<?php
-				$args = array(
-					'posts_per_page'	=> -1,
-					'post_type'			=> 'wp_locations'
-				);
-				$locations = get_posts($args);
-				if($locations){
-					foreach($locations as $location){
-						if($location->ID == $location_id){
-							echo '<option selected value="' . $location->ID . '">' . get_the_title($location->ID) . '</option>';
-						}else{
-							echo '<option value="' . $location->ID . '">' . get_the_title($location->ID) . '</option>';
-						}
-					}
-				}
-				?>
-			</select>
-		</p>
-		<p>
-			<small>If you want to display multiple locations select how many below</small><br/>
-			<label for="<?php echo $this->get_field_id('number_of_locations'); ?>">Number of Locations</label>
-			<select class="widefat" name="<?php echo $this->get_field_name('number_of_locations'); ?>" id="<?php echo $this->get_field_id('number_of_locations'); ?>" value="<?php echo $number_of_locations; ?>">
-				<option value="default">All</option>
-			</select>
-		</p>
-		<?php
-	}
-	
-	//handles updating the widget 
-	//$new_instance - new values, $old_instance - old saved values
-	public function update($new_instance, $old_instance){
-		$instance = array();
-		
-		$instance['location_id'] = $new_instance['location_id'];
-		$instance['number_of_locations'] = $new_instance['number_of_locations'];
-		
-		return $instance;
-	}
-	
-	//registers our widget for use
-	public function register_wp_location_widgets(){
-		register_widget('wp_location_widget');
-	}
+class featured_circle_widget extends WP_widget{
+//initialise widget values
+public function __construct(){
+    //set base values for the widget (override parent)
+    parent::__construct(
+        'wp_circle_widget',
+        'Featured Circles Widget', 
+        array('description' => 'A widget that displays your featured circles')
+    );
+    add_action('widgets_init',array($this,'register_wp_circle_widgets'));
 }
-$wp_location_widget = new wp_location_widget;
+
+//handles the back-end admin of the widget
+    //$instance - saved values for the form
+    public function form($instance){
+        //collect variables 
+        $circle_id = (isset($instance['circle_id']) ? $instance['circle_id'] : 'default');
+        $number_of_circles = (isset($instance['number_of_circles']) ? $instance['number_of_circles'] : 5);
+
+        ?>
+        <p>Select your options below</p>
+        <p>
+            <label for="<?php echo $this->get_field_name('circle_id'); ?>">Circle to display</label>
+            <select class="widefat" name="<?php echo $this->get_field_name('circle_id'); ?>" id="<?php echo $this->get_field_id('circle_id'); ?>" value="<?php echo $circle_id; ?>">
+                <option value="default">All Circles</option>
+                <?php
+                $args = array(
+                    'posts_per_page'    => -1,
+                    'post_type'         => 'wp_featuured_circles'
+                );
+                $circles = get_posts($args);
+                if($circles){
+                    foreach($circles as $circle){
+                        if($circle->ID == $circle_id){
+                            echo '<option selected value="' . $circle->ID . '">' . get_the_title($circle->ID) . '</option>';
+                        }else{
+                            echo '<option value="' . $circle->ID . '">' . get_the_title($circle->ID) . '</option>';
+                        }
+                    }
+                }
+                ?>
+            </select>
+        </p>
+        <p>
+            <small>If you want to display multiple circless select how many below</small><br/>
+            <label for="<?php echo $this->get_field_id('number_of_circles'); ?>">Number of Circles</label>
+            <select class="widefat" name="<?php echo $this->get_field_name('number_of_circles'); ?>" id="<?php echo $this->get_field_id('number_of_circles'); ?>" value="<?php echo $number_of_circles; ?>">
+                <option value="default" <?php if($number_of_circles == 'default'){ echo 'selected';}?>>All Circles</option>
+                <option value="1" <?php if($number_of_circles == '1'){ echo 'selected';}?>>1</option>
+                <option value="2" <?php if($number_of_circles == '2'){ echo 'selected';}?>>2</option>
+                <option value="3" <?php if($number_of_circles == '3'){ echo 'selected';}?>>3</option>
+                <option value="4" <?php if($number_of_circles == '4'){ echo 'selected';}?>>4</option>
+                <option value="5" <?php if($number_of_circles == '5'){ echo 'selected';}?>>5</option>
+                <option value="10" <?php if($number_of_circles == '10'){ echo 'selected';}?>>10</option>
+            </select>
+        </p>
+        <?php
+    }
+
+	//handles updating the widget 
+//$new_instance - new values, $old_instance - old saved values
+public function update($new_instance, $old_instance){
+
+    $instance = array();
+
+    $instance['circle_id'] = $new_instance['circle_id'];
+    $instance['number_of_circles'] = $new_instance['number_of_circles'];
+
+    return $instance;
+}
+	
+//handles public display of the widget
+//$args - arguments set by the widget area, $instance - saved values
+public function widget( $args, $instance ) {
+
+    //get wp_simple_location class (as it builds out output)
+    global $featured_circles;
+
+    //pass any arguments if we have any from the widget
+    $arguments = array();
+    //if we specify a location
+
+    //if we specify a single location
+    if($instance['circle_id'] != 'default'){
+        $arguments['circle_id'] = $instance['circle_id'];
+    }
+    //if we specify a number of locations
+    if($instance['number_of_circles'] != 'default'){
+        $arguments['number_of_circles'] = $instance['number_of_circles'];
+    }
+
+    $test = $wp_featured_circles->get_circles_output($arguments);
+	
+    //get the output
+    $html = '';
+
+    $html .= $args['before_widget'];
+    $html .= '<div class="widget">';
+    $html .= $args[before_title];
+    $html .= "Circles";
+    $html .= $args["after_title"];
+    //uses the main output function of the location class
+	if ($test != "") {
+    $html .= $featured_circles->get_circles_output($arguments);}
+	else {$html .= "no output";}
+    $html .= '</div>';
+    $html .= $args['after_widget'];
+
+    echo $html;
+}
+	
+//registers our widget for use
+public function register_wp_circle_widgets(){
+    register_widget('wp_circle_widget');
+}
+	
+}
+
+$featured_circle_widget = new featured_circle_widget_circle_widget;
 ?>
-© 2018 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-API
-Training
-Shop
-Blog
-About
-Press h to open a hovercard with more details.
